d061f522cc8a19d8285884cb749234f40ec06989
chenhua <chenhua22@outlook.com>
2017-07-17 15:33:08 +0800

    添加撸平二叉树题

---
+++ tree/Flatten_Binary_Tree_to_Linked_List.py
@@ -0,0 +1,55 @@
+# _*_ coding: utf-8 _*_
+
+'''
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        先序遍历
+        """
+        if not root:
+            return root
+        stack = [root]
+        pre = TreeNode(None)
+        while stack:
+            node = stack.pop()
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+            pre.right = node
+            pre.left = None
+            pre = node
