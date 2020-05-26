994837bae0fc2d34c5e2f5e98cfb5acb1de680a5
buhuipao <chenhua22@outlook.com>
2020-05-26 21:06:44 +0800

    260. 只出现一次的数字 III

---
+++ 2020/0260-single-number-iii.go
@@ -0,0 +1,21 @@
+func singleNumber(nums []int) []int {
+    // 找到两个不同值的异或值
+    x := 0
+    for i := range nums {
+        x ^= nums[i]
+    }
+    if x == 0 {
+        return nil
+    }
+    // 找到两个值的第一个低差异位
+    y := x - x & (x - 1)
+    var a, b int
+    for i := range nums {
+        if nums[i] & y != 0 {
+            a ^= nums[i]
+        } else {
+            b ^= nums[i]
+        }
+    }
+    return []int{a, b}
+}
