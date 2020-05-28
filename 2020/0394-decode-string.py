c25c1d7b3826b599cdc2e355c64768653230631e
buhuipao <chenhua22@outlook.com>
2020-05-28 08:39:16 +0800

    每日一题（05-28）:394. 字符串解码

---
+++ 2020/0394-decode-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack, res, repeat = [], "", 0
+        for i in s:
+            if i == '[':
+                stack.append([repeat, res])     # 保存当前的repeat和上次的结余
+                res, repeat = "", 0
+            elif i == ']':
+                cur_preat, last_res = stack.pop()   # 重新计算res
+                res = last_res + cur_preat * res
+            elif '0' <= i <= '9':
+                repeat = repeat * 10 + int(i)   # repeat可能大于10
+            else:
+                res += i
+        return res
+
+
