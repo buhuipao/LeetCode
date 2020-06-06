8abca1eb5876f32b92713aa9d23f366ac98c6ea4
buhuipao <chenhua22@outlook.com>
2020-06-06 08:29:13 +0800

    每日一题（06-06）：128. 最长连续序列

--- 2020/0128-longest-consecutive-sequence.go
+++ 2020/0128-longest-consecutive-sequence.go
@@ -1,39 +1,22 @@
-// Hash表 或者 并查集
 func longestConsecutive(nums []int) int {
-    // 特判
-    n := len(nums)
-    if n == 0 {
-        return 0
-    }
-    // 记录所有值
-    record := make(map[int]bool, n)
+    m := make(map[int]bool, len(nums))
     for i := range nums {
-        record[nums[i]] = true
+        m[nums[i]] = true
     }
     ans := 0
-    MAX_INT, MIN_INT := int(^uint(0)>>1), ^int(^uint(0)>>1)
-    for i := range nums {
-        // 如果nums[i]-1也在记录里，那么nums[i]一定会被串起啦，所以不需要参与下面的计算
-        if nums[i] != MIN_INT {
-            if _, ok := record[nums[i]-1]; ok {
-                continue
-            }
+    for k := range m {
+        // 如果有比当前值更小的，就以更小值为准
+        if _, ok := m[k-1]; ok {
+            continue
         }
-        // 统计连续序列
-        cur := 1
-        curV := nums[i]
-        for curV != MAX_INT {
-            curV++
-            if _, ok := record[curV]; ok {
-                cur++
-            } else {
-                break
-            }
+        tmp := 1
+        for m[k+1] {
+            tmp++
+            k++
         }
-        if cur > ans {
-            ans = cur
+        if tmp > ans {
+            ans = tmp
         }
     }
-
     return ans
 }
