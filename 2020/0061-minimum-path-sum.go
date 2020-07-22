24397b38ed77576eec2b79f49d05e31b9766ad77
buhuipao <chenhua22@outlook.com>
2020-07-23 07:51:26 +0800

    每日一题（07-23）:64. 最小路径和

---
+++ 2020/0061-minimum-path-sum.go
@@ -0,0 +1,28 @@
+func minPathSum(grid [][]int) int {
+    m := len(grid)
+    if m == 0 {
+        return 0
+    }
+    n := len(grid[0])
+    // 计算左边
+    for i := 1; i < m; i++ {
+        grid[i][0] += grid[i-1][0]
+    }
+    // 计算最上边
+    for j := 1; j < n; j++ {
+        grid[0][j] += grid[0][j-1]
+    }
+    for i := 1; i < m; i++ {
+        for j := 1; j < n; j++ {
+            grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        }
+    }
+    return grid[m-1][n-1]
+}
+
+func min(a, b int) int {
+    if a > b {
+        return b
+    }
+    return a
+}
