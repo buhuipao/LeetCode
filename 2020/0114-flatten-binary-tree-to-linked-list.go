442fb1832fa6339d706de4a90446265bb2d2adff
buhuipao <chenhua22@outlook.com>
2020-08-02 11:22:51 +0800

    每日一题（08-02）:114. 二叉树展开为链表

--- 2020/0114-flatten-binary-tree-to-linked-list.go
+++ 2020/0114-flatten-binary-tree-to-linked-list.go
@@ -6,14 +6,13 @@
  *     Right *TreeNode
  * }
  */
- // 先序遍历
 func flatten(root *TreeNode)  {
     if root == nil {
         return
     }
     stack := []*TreeNode{root}
     pre := &TreeNode{}
-    for len(stack) > 0 {
+    for len(stack) != 0 {
         node := stack[len(stack)-1]
         stack = stack[:len(stack)-1]
         if node.Right != nil {
@@ -22,8 +21,8 @@ func flatten(root *TreeNode)  {
         if node.Left != nil {
             stack = append(stack, node.Left)
         }
-        // 清理
         node.Left, node.Right = nil, nil
-        pre, pre.Right = node, node
+        pre.Right, pre = node, node
     }
+
 }
