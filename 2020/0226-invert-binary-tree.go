960c150e8322fa2228e64efc1a092abc47f01a5f
buhuipao <chenhua22@outlook.com>
2020-09-16 23:02:42 +0800

    每日一题（09-16）:226. 翻转二叉树

--- 2020/0226-invert-binary-tree.go
+++ 2020/0226-invert-binary-tree.go
@@ -6,23 +6,13 @@
  *     Right *TreeNode
  * }
  */
- // 按层遍历同时反转
 func invertTree(root *TreeNode) *TreeNode {
     if root == nil {
         return nil
     }
-    queue := []*TreeNode{root}
-    for len(queue) != 0 {
-        node := queue[0]
-        queue = queue[1:]
-        node.Left, node.Right = node.Right, node.Left
-        if node.Left != nil {
-            queue = append(queue, node.Left)
-        }
-        if node.Right != nil {
-            queue = append(queue, node.Right)
-        }
+    if root.Left == nil && root.Right == nil {
+        return root
     }
-
+    root.Left, root.Right = invertTree(root.Right), invertTree(root.Left)
     return root
 }
