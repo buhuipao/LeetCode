ca2e4ef5d04ef0394543248259f5cc67c137a4d2
buhuipao <chenhua22@outlook.com>
2020-09-20 10:36:16 +0800

    每日一题（09-20）：78. 子集

---
+++ 2020/0078-subsets.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for i in nums:
+            res = res + [[i] + num for num in res]
+        return res
