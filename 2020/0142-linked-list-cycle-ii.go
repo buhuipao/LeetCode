874c6c56522153c2b6c66f75a0c605180e79640b
buhuipao <chenhua22@outlook.com>
2020-10-10 07:55:03 +0800

    每日一题（10-10）：142. 环形链表 II

---
+++ 2020/0142-linked-list-cycle-ii.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+    fast, slow := head, head    
+    var node *ListNode
+    for fast != nil && fast.Next != nil {
+        fast, slow = fast.Next.Next, slow.Next
+        if fast == slow {
+            node = fast
+            break
+        }
+    }
+    // 无环
+    if node == nil {
+        return nil
+    }
+    fast = head
+    for fast != slow {
+        fast, slow = fast.Next, slow.Next
+    }
+    return fast
+}
