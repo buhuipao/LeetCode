2a507b317861dc03f5fd5fe244c7004cca6c7399
buhuipao <chenhua22@outlook.com>
2020-11-16 22:38:27 +0800

    每日一题（11-16）：406. 根据身高重建队列

--- 2020/0406-queue-reconstruction-by-height.py
+++ 2020/0406-queue-reconstruction-by-height.py
@@ -1,6 +1,7 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
         # 按照people[0]降序排列，同时people[1]升序排列
+        # 降序排列可以使得每次的插入都忽略其身后的（已插入）的人
         people = sorted(people, key=lambda p: (-p[0], p[1]))
         ans = []
         for p in people:
