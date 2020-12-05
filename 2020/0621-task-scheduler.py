1b59bc2e1fbde258bdc1ee25aca70c4df9bd7072
buhuipao <chenhua22@outlook.com>
2020-12-05 22:37:27 +0800

    每日一题（12-05）：621. 任务调度器

---
+++ 2020/0621-task-scheduler.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        """
+        :type tasks: List[str]
+        :type n: int
+        :rtype: int
+        """
+        length = len(tasks)
+        if length <= 1:
+            return length
+        
+        # 用于记录每个任务出现的次数
+        task_map = dict()
+        for task in tasks:
+            task_map[task] = task_map.get(task, 0) + 1
+        # 按任务出现的次数从大到小排序
+        task_sort = sorted(task_map.items(), key=lambda x: x[1], reverse=True)
+        
+        # 出现最多次任务的次数
+        max_task_count = task_sort[0][1]
+        # 至少需要的最短时间
+        res = (max_task_count - 1) * (n + 1)
+        
+        for sort in task_sort:
+            # 此处的理解：
+            # 举例：n为2
+            # A, A, A, B, B, B, C => A, (B), (C), A, (B), (), A, B
+            # 如果出现了与最大频率相同的任务（例如 B），则会被追加到最后末尾（包括A自己）
+            if sort[1] == max_task_count:
+                res += 1
+        
+        # 如果结果比任务数量少，则返回总任务数
+        return res if res >= length else length
