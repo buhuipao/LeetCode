a28d5657af0fde490ea52e40d5f75941c16dd480
buhuipao <chenhua22@outlook.com>
2020-12-14 23:10:44 +0800

    每日一题（12-14）：49. 字母异位词分组

--- 2020/0049-group-anagrams.py
+++ 2020/0049-group-anagrams.py
@@ -4,6 +4,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         record = defaultdict(list)
         for s in strs:
+            # 排序
             key = ''.join(sorted(s))
             record[key].append(s)
 
