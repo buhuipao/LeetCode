fc5d722e5153722a68b39ed006cd83920c542903
buhuipao <chenhua22@outlook.com>
2021-03-12 23:20:00 +0800

    每日一题（03-12）：331. 验证二叉树的前序序列化

---
+++ 2021/0331-verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isValidSerialization(self, preorder):
+        stack = []
+        for node in preorder.split(','):
+            stack.append(node)
+            while len(stack) >= 3 and stack[-1] == stack[-2] == '#' and stack[-3] != '#':
+                stack.pop(), stack.pop(), stack.pop()
+                stack.append('#')
+        return len(stack) == 1 and stack.pop() == '#'
