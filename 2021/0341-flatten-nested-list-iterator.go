2392aecaf6544048de661dfc0c0628f49300b2fe
buhuipao <chenhua22@outlook.com>
2021-03-23 22:46:44 +0800

    每日一题（03-23）：341. 扁平化嵌套列表迭代器

---
+++ 2021/0341-flatten-nested-list-iterator.go
@@ -0,0 +1,55 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * type NestedInteger struct {
+ * }
+ *
+ * // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ * func (this NestedInteger) IsInteger() bool {}
+ *
+ * // Return the single integer that this NestedInteger holds, if it holds a single integer
+ * // The result is undefined if this NestedInteger holds a nested list
+ * // So before calling this method, you should have a check
+ * func (this NestedInteger) GetInteger() int {}
+ *
+ * // Set this NestedInteger to hold a single integer.
+ * func (n *NestedInteger) SetInteger(value int) {}
+ *
+ * // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ * func (this *NestedInteger) Add(elem NestedInteger) {}
+ *
+ * // Return the nested list that this NestedInteger holds, if it holds a nested list
+ * // The list length is zero if this NestedInteger holds a single integer
+ * // You can access NestedInteger's List element directly if you want to modify it
+ * func (this NestedInteger) GetList() []*NestedInteger {}
+ */
+
+type NestedIterator struct {
+    vals []int
+}
+
+func Constructor(nestedList []*NestedInteger) *NestedIterator {
+    var vals []int
+    var dfs func([]*NestedInteger)
+    dfs = func(nestedList []*NestedInteger) {
+        for _, nest := range nestedList {
+            if nest.IsInteger() {
+                vals = append(vals, nest.GetInteger())
+            } else {
+                dfs(nest.GetList())
+            }
+        }
+    }
+    dfs(nestedList)
+    return &NestedIterator{vals}
+}
+
+func (it *NestedIterator) Next() int {
+    val := it.vals[0]
+    it.vals = it.vals[1:]
+    return val
+}
+
+func (it *NestedIterator) HasNext() bool {
+    return len(it.vals) > 0
+}
