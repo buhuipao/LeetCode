c01c6ac29529dc8c6e97ed59cec40bb7679e4ef7
buhuipao <chenhua22@outlook.com>
2021-04-25 16:39:57 +0800

    每日一题（04-25）：897. 递增顺序搜索树

---
+++ 2021/0897-ncreasing-order-search-tree.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ 
+func increasingBST(root *TreeNode) *TreeNode {
+    stack, node := []*TreeNode{}, root
+    var pre, newRoot *TreeNode
+    for len(stack) != 0 || node != nil {
+        for node != nil {
+            stack = append(stack, node)
+            node = node.Left
+        }
+        node = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if newRoot == nil {
+            newRoot = node
+        }
+        if pre != nil {
+            pre.Right = node
+        }
+        node, pre = node.Right, node
+    }
+    return newRoot
+}
