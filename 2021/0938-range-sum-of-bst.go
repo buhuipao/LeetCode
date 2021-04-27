7106b73e8093e17f2b7cb334fe4570a7d5887373
buhuipao <chenhua22@outlook.com>
2021-04-27 19:15:54 +0800

    每日一题（04-27）：938. 二叉搜索树的范围和

---
+++ 2021/0938-range-sum-of-bst.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    var ans int
+    stack := []*TreeNode{}
+    for len(stack) != 0 || root != nil {
+        for root != nil {
+            stack = append(stack, root)
+            root = root.Left
+        }
+        root = stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if root.Val >= low && root.Val <= high {
+            ans += root.Val
+        }
+        root = root.Right
+    }
+    return ans
+}
