f0d6d5bcc192fa1abd81ea9095cab79659a3d09e
buhuipao <chenhua22@outlook.com>
2021-07-16 23:06:34 +0800

    每日一题（07-16）：剑指 Offer 53 - I. 在排序数组中查找数字 I

---
+++ 2021/lcof-53-zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof.go
@@ -0,0 +1,9 @@
+// 二分查找
+func search(nums []int, target int) int {
+    leftmost := sort.SearchInts(nums, target)
+    if leftmost == len(nums) || nums[leftmost] != target {
+        return 0
+    }
+    rightmost := sort.SearchInts(nums, target + 1) - 1
+    return rightmost - leftmost + 1
+}
