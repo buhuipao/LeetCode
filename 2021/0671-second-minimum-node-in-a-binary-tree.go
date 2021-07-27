9c41d261ad3853516c779d0f45d9b94f4cb795bf
buhuipao <chenhua22@outlook.com>
2021-07-27 22:45:16 +0800

    每日一题（07-27）：671. 二叉树中第二小的节点

---
+++ 2021/0671-second-minimum-node-in-a-binary-tree.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findSecondMinimumValue(root *TreeNode) int {
+    ans := -1
+    rootVal := root.Val
+    var dfs func(*TreeNode)
+    dfs = func(node *TreeNode) {
+        if node == nil || ans != -1 && node.Val >= ans {
+            return
+        }
+        if node.Val > rootVal {
+            ans = node.Val
+        }
+        dfs(node.Left)
+        dfs(node.Right)
+    }
+    dfs(root)
+    return ans
+}
