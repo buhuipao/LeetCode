ebba8659fce8e255bcc9faf05804920a70fe16de
buhuipao <chenhua22@outlook.com>
2021-08-25 23:46:50 +0800

    每日一题（08-25）：797. 所有可能的路径

---
+++ 2021/0797-all-paths-from-source-to-target.go
@@ -0,0 +1,18 @@
+// DFS
+func allPathsSourceTarget(graph [][]int) (ans [][]int) {
+    stk := []int{0}
+    var dfs func(int)
+    dfs = func(x int) {
+        if x == len(graph)-1 {
+            ans = append(ans, append([]int(nil), stk...))
+            return
+        }
+        for _, y := range graph[x] {
+            stk = append(stk, y)
+            dfs(y)
+            stk = stk[:len(stk)-1]
+        }
+    }
+    dfs(0)
+    return
+}
