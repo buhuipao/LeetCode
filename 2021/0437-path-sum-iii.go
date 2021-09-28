63ce7741eb5939297926d988a9fbe897b0c49e20
buhuipao <chenhua22@outlook.com>
2021-09-28 23:35:35 +0800

    每日一题（09-28）：437. 路径总和 III

---
+++ 2021/0437-path-sum-iii.go
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func pathSum(root *TreeNode, sum int) int {
+    if root == nil {
+        return 0
+    }
+    
+    var a, b, c int
+    a = helper(root, sum)
+    b = pathSum(root.Left, sum)
+    c = pathSum(root.Right, sum)
+
+    return a + b + c
+}
+
+// 以root节点为起始位置和为target 的数量
+func helper(root *TreeNode, target int) int {
+    if root == nil {
+        return 0
+    }
+
+    var c int
+    if root.Val == target {
+        c = 1
+    }
+
+    return c + helper(root.Left, target-root.Val) + helper(root.Right, target-root.Val)
+}
