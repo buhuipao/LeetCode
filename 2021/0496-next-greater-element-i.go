1181fe88ab73d66ae7512ff1924f0eb53c289ca6
buhuipao <chenhua22@outlook.com>
2021-10-26 23:46:35 +0800

    每日一题（10-26）：496. 下一个更大元素 I

---
+++ 2021/0496-next-greater-element-i.go
@@ -0,0 +1,24 @@
+// 单调栈
+func nextGreaterElement(nums1, nums2 []int) []int {
+    mp := map[int]int{}
+    stack := []int{}
+    for i := len(nums2) - 1; i >= 0; i-- {
+        num := nums2[i]
+        for len(stack) > 0 && num >= stack[len(stack)-1] {
+            stack = stack[:len(stack)-1]
+        }
+        if len(stack) > 0 {
+            mp[num] = stack[len(stack)-1]
+        } else {
+            mp[num] = -1
+        }
+        stack = append(stack, num)
+    }
+
+    res := make([]int, len(nums1))
+    for i, num := range nums1 {
+        res[i] = mp[num]
+    }
+    
+    return res
+}
