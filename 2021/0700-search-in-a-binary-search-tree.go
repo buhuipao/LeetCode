a92481c17a7a2b32724e3d60570fe5a855b28a66
buhuipao <chenhua22@outlook.com>
2021-11-26 22:53:30 +0800

    每日一题（11-26）：700. 二叉搜索树中的搜索

---
+++ 2021/0700-search-in-a-binary-search-tree.go
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+    if root == nil || root.Val == val{
+        return root
+    }
+
+    if val > root.Val {
+        return searchBST(root.Right, val)
+    }
+
+    return searchBST(root.Left, val)
+}
