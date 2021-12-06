546100c4117f638f4218b4658a4e5c994764720a
buhuipao <chenhua22@outlook.com>
2021-12-06 23:09:47 +0800

    每日一题（12-06）：1816. 截断句子

---
+++ 2021/1816-truncate-sentence.go
@@ -0,0 +1,13 @@
+func truncateSentence(s string, k int) string {
+    for i := range s {
+        if s[i] == ' ' {
+            k--
+        } 
+
+        if k == 0 {
+            return s[:i]
+        }
+    }
+
+    return s 
+}
