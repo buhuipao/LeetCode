f43b9a8755c9802a534230564dcc942eb3908e62
buhuipao <chenhua22@outlook.com>
2022-06-14 06:52:10 -0700

    144. 二叉树的前序遍历

---
+++ category/tree/0144-binary-tree-preorder-traversal.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+/*
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+
+    return append(append([]int{root.Val}, preorderTraversal(root.Left)...), preorderTraversal(root.Right)...)
+}
+*/
+
+func preorderTraversal(root *TreeNode) []int {
+    if root == nil {
+        return nil
+    }
+
+    var stack []*TreeNode
+    stack = append(stack, root)
+    var ans []int
+    for len(stack) != 0 {
+        node := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        ans = append(ans, node.Val)
+        
+        if node.Right != nil {
+            stack = append(stack, node.Right)
+        }
+
+        if node.Left != nil {
+            stack = append(stack, node.Left)
+        }
+    }
+
+    return ans
+}
